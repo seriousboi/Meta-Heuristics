ed345decf4620a6ad9dc86296eb12caa214dd17b
seriousboi <ppinet9@gmail.com>
2023-04-24 06:50:24 +0200

    Optimums et temps pour quelques instances.

---
+++ optimums.py
@@ -0,0 +1,12 @@
+
+
+
+optimums = {
+2: {'dixSommets.txt': 12, 'quinzeSommets.txt': 43, 'dixSeptSommets.txt': 60, 'vingtSommets.txt': 39, 'vingtEtunSommets.txt': 107, 'vingtDeuxSommets.txt': 102, 'vingtTroisSommets.txt': 116, 'vingtQuatreSommets.txt': 129}
+}
+
+
+
+times = {
+2: {'dixSommets.txt': 0.0066964339930564165, 'quinzeSommets.txt': 0.21682916399731766, 'dixSeptSommets.txt': 0.9540450159984175, 'vingtSommets.txt': 9.495417890997487, 'vingtEtunSommets.txt': 22.599431626003934, 'vingtDeuxSommets.txt': 61.78928277999512, 'vingtTroisSommets.txt': 81.92406463999941, 'vingtQuatreSommets.txt': 228.24343598200358}
+}
