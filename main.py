4c52d8f957b210fabeddabd1f4ecc796d6e90382
seriousboi <ppinet9@gmail.com>
2023-04-24 02:25:18 +0200

    Préparation aux tests.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from neighborhoods import swapNeighborhood, pickNDropNeighborhood
-from tests import testGradient, testExhaustive, testSimulatedAnnealing, testTabu, testImplicit
+from tests import *
 
 nbClasses = 2
 equityMax = 2
@@ -10,8 +10,11 @@ nbChangeTemperature = 50
 nbIterMaxFunction = lambda n : n**2 / 2 # with n the size of our problem.
 tabuListSize = 7
 
-# testExhaustive(nbClasses, equityMax, maxTime)
+#testExhaustive(nbClasses, equityMax, maxTime)
 # testGradient(nbClasses, equityMax, swapNeighborhood, maxTime)
-testSimulatedAnnealing(nbClasses, equityMax, pickNDropNeighborhood, initialTemperature, nbChangeTemperature, MU, nbIterMaxFunction, maxTime)
+# testSimulatedAnnealing(nbClasses, equityMax, pickNDropNeighborhood, initialTemperature, nbChangeTemperature, MU, nbIterMaxFunction, maxTime)
 # testTabu(nbClasses, equityMax, pickNDropNeighborhood, tabuListSize, nbIterMaxFunction, maxTime)
 # testImplicit(nbClasses, equityMax, maxTime)
+
+
+getOptimums(300)
--- tests.py
+++ tests.py
@@ -6,6 +6,7 @@ from gradients import gradient, getRandomSolution
 from simulatedAnnealing import simulatedAnnealingSimulation
 from tabu import tabuSimulation
 from implicit import implicitSearch
+from optimums import *
 from os import listdir
 
 folder = "data"
@@ -16,7 +17,76 @@ filenames = [
 'trenteSommets.txt','cinquanteSommets.txt','centSommets.txt','cinqCentSommets.txt','milleSommets.txt'
 ]
 
-# filenames = listdir(folder)
+goodFilenames = ['dixSommets.txt','quinzeSommets.txt','dixSeptSommets.txt',
+'vingtSommets.txt','vingtEtunSommets.txt','vingtDeuxSommets.txt','vingtTroisSommets.txt','vingtQuatreSommets.txt',]
+
+
+
+def testEverything(maxTime,startStep = 0):
+    step = 0
+
+    for nbClasses in [2,4,8]:
+        for filename in goodFilenames:
+
+            if step >= startStep:
+                graph = loadGraph(f"{folder}/{filename}")
+                print("instance",filename)
+                print(graph.nbVertices, "vertices to split in",nbClasses,"classes")
+
+                print(testAllMethodsOnInstance(graph,nbClasses,maxTime))
+
+                print(step+1,"steps done")
+                print()
+
+            step += 1
+
+
+
+def testAllMethodsOnInstance(graph,nbClasses,maxTime):
+    equityMax = max(2,int((graph.nbVertices/nbClasses)/10)) #équité à 10% près de la taille d'une classe moyenne
+
+    gradientSolution,timeTaken = gradient(graph,nbClasses, neighborhoodFunction, maxTime, nbImprovToBreak)
+
+    gradientSolution,timeTaken = gradient(graph,nbClasses, neighborhoodFunction, maxTime, nbImprovToBreak)
+
+    simulatedAnnealingSolution, simulatedAnnealingValue, timeTaken = simulatedAnnealingSimulation(graph, nbClasses, equityMax, neighborhoodFunction, \
+    initialTemperature, shutdownTemperature, g, nbIterMax, maxTime)
+
+    tabuSolution, tabuValue, timeTaken = tabuSimulation(graph, nbClasses, equityMax, neighborhoodFunction, tabuListSize, nbIterMax, maxTime)
+
+
+    return 
+
+
+
+def getOptimums(maxTime):
+    optimums = {}
+    times = {}
+
+    for nbClasses in [2,4,8]:
+        optimums[nbClasses] = {}
+        times[nbClasses] = {}
+
+        for filename in goodFilenames:
+            graph = loadGraph(f"{folder}/{filename}")
+            equityMax = max(2,int((graph.nbVertices/nbClasses)/10))
+            print("instance",filename)
+            print(graph.nbVertices, "vertices to split in",nbClasses,"classes")
+
+            implicitSolution, implicitValue, timeTaken = implicitSearch(graph, nbClasses, equityMax, maxTime)
+
+            if timeTaken < maxTime:
+                optimums[nbClasses][filename] = implicitValue
+                times[nbClasses][filename] = timeTaken
+            else:
+                optimums[nbClasses][filename] = None
+                times[nbClasses][filename] = maxTime
+
+            print(optimums)
+            print(times)
+            print()
+
+
 
 def testExhaustive(nbClasses,equityMax,maxTime = None,maxIterations = None):
     global filenames
@@ -43,7 +113,6 @@ def testExhaustive(nbClasses,equityMax,maxTime = None,maxIterations = None):
             print(exhaustiveSolution)
             if not checkSolution(exhaustiveSolution,nbClasses,True,equityMax):
                 print("/!\ invalid solution /!\ ")
-
         print()
 
 
@@ -66,6 +135,7 @@ def testGradient(nbClasses,equityMax,neighborhoodFunction,maxTime,nbImprovToBrea
         if not checkSolution(gradientSolution,nbClasses,True,equityMax):
             print("/!\ invalid solution /!\ ")
         print()
+
 
 
 def testSimulatedAnnealing(nbClasses, equityMax, neighborhoodFunction, initialTemperature, nbChangeTemperature, MU, nbIterMaxFunction, maxTime):
@@ -96,6 +166,7 @@ def testSimulatedAnnealing(nbClasses, equityMax, neighborhoodFunction, initialTe
         print('\n')
 
 
+
 def testTabu(nbClasses, equityMax, neighborhoodFunction, tabuListSize, nbIterMaxFunction, maxTime):
     """
     Function to launch tabu search algorithm over the files.
@@ -118,6 +189,7 @@ def testTabu(nbClasses, equityMax, neighborhoodFunction, tabuListSize, nbIterMax
             print("/!\ invalid solution /!\ \n")
 
         print('\n')
+
 
 
 def testImplicit(nbClasses, equityMax, maxTime):
